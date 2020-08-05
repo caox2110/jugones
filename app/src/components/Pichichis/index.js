9e2271b0c6e8472b4a73d6d678e481329cbe77fd
Frank González Hernández <frank.gonzalezhernandez@amaris.com>
2020-08-05 17:59:17 +0200

    Ejercicio 7. Agregado botón para ordenar listado de pichichis Ascendente y descendente. Se utilizo un caracter pues no se encontró el diseño con la flecha.

--- app/src/components/Pichichis/index.js
+++ app/src/components/Pichichis/index.js
@@ -18,6 +18,7 @@ function Pichichis({
     const [loading, setLoading] = useState(false)
     const [hasError, setHasError] = useState(false)
     const [pichichis, setPichichis] = useState([])
+    const [direction, setDirection] = useState('sorterDown')
 
     const getPichichisAction = async () => {
         setLoading(true)
@@ -28,9 +29,23 @@ function Pichichis({
         } catch (err) {
             console.log(`El error es: ${err}`)
             setHasError(true)
-            setPichichis([])
         }
         setLoading(false)
+    }
+
+    const sorterAction = (e) => {
+        e.preventDefault()
+        let results = []
+        switch (direction) {
+            case 'sorterUp':
+                results = [...pichichis].sort((p1, p2) => { return p1.goals - p2.goals })
+                break
+            default:
+                results = [...pichichis].sort((p1, p2) => { return p2.goals - p1.goals })
+                break
+        }
+        setDirection(direction === 'sorterDown' ? 'sorterUp' : 'sorterDown')
+        setPichichis(results)
     }
 
     useEffect(
@@ -52,7 +67,16 @@ function Pichichis({
                         <thead>
                             <tr className={styles.tr}>
                                 <th className={styles.th}>Nombre</th>
-                                <th className={styles.th}>Goles</th>
+                                <th className={`${styles.th} `}>
+                                    Goles
+                                    <a
+                                        href='#!'
+                                        className={`${styles[direction]}`}
+                                        onClick={sorterAction}
+                                    >
+                                        &nbsp;
+                                     </a>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
